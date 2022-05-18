09a664c869e3b70df9bd343e0564fd1fe4a7acfd
sahanasai11 <97415870+sahanasai11@users.noreply.github.com>
2022-05-17 23:25:00 -0700

    Add files via upload

---
+++ scaled_vgg.py
@@ -0,0 +1,246 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+from numpy import asarray
+import PIL
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import cv2
+
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Dropout, BatchNormalization, MaxPooling2D, Conv2D
+
+# -----------------------------------------------------------------------
+# DATA UPLOAD
+# -----------------------------------------------------------------------  
+label_names = ["No Finding","Enlarged Cardiomediastinum","Cardiomegaly",
+               "Lung Opacity","Lung Lesion","Edema","Consolidation","Pneumonia",
+               "Atelectasis","Pneumothorax","Pleural Effusion","Pleural Other",
+               "Fracture","Support Devices"]
+
+id_header = 'Id,No Finding,Enlarged Cardiomediastinum,Cardiomegaly,Lung Opacity,Lung Lesion,Edema,Consolidation,Pneumonia,Atelectasis,Pneumothorax,Pleural Effusion,Pleural Other,Fracture,Support Devices'
+ 
+# -----------------------------------------------------------------------
+# FOR ROOT PATHS: EITHER COMMENT OUT BLOCK USING 100 x 100 or 224 x 224
+# -----------------------------------------------------------------------
+# width = 100
+# height = 100   
+
+## subset 20,000 training data ( 100 x 100 )
+# TRAIN_PATH = '/central/groups/CS156b/teams/clnsh/subset_compressed_data20/subset_'
+
+# # full testing data ( 100 x 100 )
+# TEST_PATH = '/central/groups/CS156b/teams/clnsh/compressed_data/'
+# -----------------------------------------------------------------------
+width = 224
+height = 224
+
+# # subset 10,000 training data ( 224 x 224 )
+TRAIN_PATH ='/central/groups/CS156b/teams/clnsh/subset_compressed_224_train/'
+
+# # full testing data ( 224 x 224 )
+TEST_PATH = '/central/groups/CS156b/teams/shncl2/'
+# -----------------------------------------------------------------------
+
+# Training data:
+print('uploading data...')
+train_loaded = np.loadtxt(TRAIN_PATH + 'train_matrices')
+num_train = int(train_loaded.size / (width * height))
+train = train_loaded.reshape(num_train, (width*height) // width, width)
+
+print('train images loaded: ', len(train))
+
+train_label = pd.read_table(TRAIN_PATH + 'train_labels', delimiter=" ", 
+              names=label_names)
+
+print('train labels loaded: ', len(train_label))
+
+# Testing data:
+test_loaded = np.loadtxt(TEST_PATH + 'test_matrices') # NOTE: 224 is added
+num_test = int(test_loaded.size / (width * height))
+test = test_loaded.reshape(num_test, (width*height) // width, width)
+
+print('test images loaded: ', len(test))
+
+test_img_id = np.loadtxt(TEST_PATH + 'test_img_ids')
+
+print('test id loaded:', len(test_img_id))
+
+# Solution data:
+#solution_loaded = np.loadtxt(TEST_PATH + 'solution_matrices')
+#num_solution = int(solution_loaded.size / (width * height))
+#solution = solution_loaded.reshape(num_solution, (width*height) // width, width)
+#print('solution images loaded: ', len(solution))
+
+#solution_img_id = np.loadtxt(TEST_PATH + 'solution_img_ids')
+#print('solution id loaded: ', len(solution_img_id))
+
+
+# forming labels of training data
+y_train = []
+for index, row in train_label.iterrows():
+    temp2 = [row['No Finding'], row['Enlarged Cardiomediastinum'], row['Cardiomegaly'],
+            row['Lung Opacity'], row['Lung Lesion'], row['Edema'], row['Consolidation'],
+            row['Pneumonia'], row['Atelectasis'], row['Pneumothorax'], row['Pleural Effusion'], 
+            row['Pleural Other'], row['Fracture'], row['Support Devices']]
+    i = 0 
+    for val in temp2: 
+        if val != val: # Handles NaN's
+            temp2[i] = 0.0
+        i += 1
+    y_train.append(temp2)
+
+print('y_train loaded: ', len(y_train), '\n\n')
+X_train = train
+y_train = np.array(y_train)
+
+# scaling the labels to be 0, 0.5, 1
+y_train_scaled = copy.deepcopy(y_train)
+i = 0
+while i < len(y_train_scaled):
+    y_train_scaled[i] = (y_train_scaled[i] + 1) / 2
+    i += 1
+y_train_scaled = np.array(y_train_scaled)
+
+
+# Forming lists of tensored images
+print('initializing tensors...')
+tensor_train = []
+tensor_test = []
+tensor_solution = []
+
+for i in range(0, len(train)):
+    tensor_train.append(tf.convert_to_tensor(train[i], dtype=float))
+print('train tensors created')
+
+for i in range(0, len(test)):
+    tensor_test.append(tf.convert_to_tensor(test[i], dtype=float))
+print('test tensors created')
+
+# setting training and testing data to be a tensor of tensor-ed images
+# shape: n training images, shape width, shape height
+train_tensor_of_tensors = tf.convert_to_tensor(tensor_train)
+print('train tensor of tensor created:', train_tensor_of_tensors.shape)
+
+test_tensor_of_tensors = tf.convert_to_tensor(tensor_test)
+print('test tensors of tensors created:', test_tensor_of_tensors.shape)
+
+
+# -----------------------------------------------------------------------
+# VGG
+# -----------------------------------------------------------------------
+num_epoch = 20
+num_batch = 128
+num_classes = 14
+
+input = Input(shape =(100,100,3))
+# 1st Conv Block
+
+x = Conv2D (filters =64, kernel_size =3, padding ='same', activation='relu')(input)
+x = Conv2D (filters =64, kernel_size =3, padding ='same', activation='relu')(x)
+x = MaxPool2D(pool_size =2, strides =2, padding ='same')(x)
+# 2nd Conv Block
+
+x = Conv2D (filters =128, kernel_size =3, padding ='same', activation='relu')(x)
+x = Conv2D (filters =128, kernel_size =3, padding ='same', activation='relu')(x)
+x = MaxPool2D(pool_size =2, strides =2, padding ='same')(x)
+# 3rd Conv block
+
+x = Conv2D (filters =256, kernel_size =3, padding ='same', activation='relu')(x)
+x = Conv2D (filters =256, kernel_size =3, padding ='same', activation='relu')(x)
+x = Conv2D (filters =256, kernel_size =3, padding ='same', activation='relu')(x)
+x = MaxPool2D(pool_size =2, strides =2, padding ='same')(x)
+# 4th Conv block
+
+x = Conv2D (filters =512, kernel_size =3, padding ='same', activation='relu')(x)
+x = Conv2D (filters =512, kernel_size =3, padding ='same', activation='relu')(x)
+x = Conv2D (filters =512, kernel_size =3, padding ='same', activation='relu')(x)
+x = MaxPool2D(pool_size =2, strides =2, padding ='same')(x)
+
+# 5th Conv block
+
+x = Conv2D (filters =512, kernel_size =3, padding ='same', activation='relu')(x)
+x = Conv2D (filters =512, kernel_size =3, padding ='same', activation='relu')(x)
+x = Conv2D (filters =512, kernel_size =3, padding ='same', activation='relu')(x)
+x = MaxPool2D(pool_size =2, strides =2, padding ='same')(x)
+# Fully connected layers
+
+x = Flatten()(x)
+x = Dense(units = 4096, activation ='relu')(x)
+x = Dense(units = 4096, activation ='relu')(x)
+output = Dense(units = num_classes, activation ='softmax')(x)
+# creating the model
+
+model = Model (inputs=input, outputs =output)
+
+model.compile(
+    loss=keras.losses.MeanSquaredError(),
+    optimizer=tf.optimizers.SGD(learning_rate=0.01),
+    metrics=['mean_squared_error'])
+
+history = model.fit(train_tensor_of_tensors, y_train, epochs=num_epoch, verbose=1, batch_size=num_batch)
+preds = model.predict(test_tensor_of_tensors)
+preds = preds.tolist()
+
+# -----------------------------------------------------------------------
+# OUTPUT
+# -----------------------------------------------------------------------
+
+# combining all the predicted labels into one combined predicted array
+combined = copy.deepcopy(preds)
+
+for i in range(len(tensor_test)):
+    img_id = test_img_id[i]
+    combined[i].insert(0, img_id)
+
+# scaling the predictions to be between -1 and 1
+combined_scaled = copy.deepcopy(combined)
+
+for i in range(len(combined_scaled)):
+    row = combined_scaled[i][1:]
+    row = [((elem * 2) - 1) for elem in row]
+    for j in range(len(row)):
+        if row[j] <= -0.8:
+            row[j] = row[j] / 2
+    combined_scaled[i][1:] = row
+
+# hack-y no finding 
+for i in range(len(combined_scaled)): 
+    row = combined_scaled[i]
+    curr = row[2:]
+    flag = False
+    for val in curr: 
+        if val >= 0.2: 
+            flag = True
+            break 
+    if flag == True: 
+        combined_scaled[i][1] = -1
+print(combined_scaled[0])
+
+# saving the predictions
+np.set_printoptions(suppress=True) # Stop ID's from being converted to scientific notation
+np.savetxt("test_sub.csv", combined_scaled, delimiter=",", fmt='%f', header = id_header, comments = '')
+
+print('DONE')
+
+
+# -----------------------------------------------------------------------
+# LOSS PLOTTING
+# -----------------------------------------------------------------------
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('other classification model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('model_loss_other.png')
+plt.show()
+
